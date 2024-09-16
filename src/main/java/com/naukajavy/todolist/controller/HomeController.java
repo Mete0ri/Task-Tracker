562f3d857bc640fab975f04f30ae66c685522cb9
Mati <byku00pl@gmail.com>
2024-09-16 16:27:38 +0200

    Added frontend

---
+++ src/main/java/com/naukajavy/todolist/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.naukajavy.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "homePanel";
+    }
+}
