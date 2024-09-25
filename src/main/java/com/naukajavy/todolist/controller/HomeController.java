3fc734bbaaa145a9cb016c23e415b1f82d116913
Mati <byku00pl@gmail.com>
2024-09-25 19:22:56 +0200

    Added delete button

--- src/main/java/com/naukajavy/todolist/controller/HomeController.java
+++ src/main/java/com/naukajavy/todolist/controller/HomeController.java
@@ -4,9 +4,11 @@ import com.naukajavy.todolist.model.Task;
 import com.naukajavy.todolist.repository.TaskRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
 
 @Controller
 public class HomeController {
@@ -21,5 +23,10 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("tasks",taskRepository.findAll());
         return "homePanel";
+    }
+    @PostMapping("/delete/{id}")
+    private String deleteTask(@PathVariable Long id){
+        taskRepository.deleteById(id);
+        return "redirect:/";
     }
 }
